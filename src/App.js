af33b7b91a5a2886a0a9a32ae14479db65819ddd
kikedb <enriquediazbarrios@gmail.com>
2021-02-03 11:55:50 -0300

    Api fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 import FormularioSimple from './components/FormularioSimple';
 class App extends Component {
+  constructor() {
+    super()
+    // GET
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then(x =>x.json())
+    //   .then(x => console.log(x))
+    fetch('https://jsonplaceholder.typicode.com/users' , {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: 'Kike',
+        username: 'kikedb'
+      })
+    }).then(x => x.json())
+    .then(x => console.log(x))
+
+  }
   render () {
     return (
       <div className="App">
