23385acba616edfe1186751d5f9cc46658c9ad50
kikedb <enriquediazbarrios@gmail.com>
2021-02-03 11:19:54 -0300

    Formularios: Campos y manejando los cambios onChange

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import React, { Component } from 'react';
-import Cabecera from './components/Cabecera'
-import P from './components/P'
-import './App.css';
-
+import FormularioSimple from './components/FormularioSimple';
 class App extends Component {
-  state = {
-    miau: 'Bienvenido a miau'
-  }
-
-  cambiarTextoDelEstado = () => {
-    this.setState({miau: 'Hola mundo' })
-  }
-
-  manejaClick = texto => {
-    console.log(texto);
-  }
   render () {
-    const { miau} = this.state
-    // const texto = 'Bienvenido miau'
-    // const guau = 'Bienvenido guau'
     return (
       <div className="App">
-        <Cabecera miau={miau} manejaClick={this.manejaClick} />
-        {/* <Cabecera miau={guau} manejaClick={this.manejaClick} /> */}
-        <P onClick={this.cambiarTextoDelEstado}>
-          {miau}
-        </P>
+        <FormularioSimple />
       </div>
     )
   }
